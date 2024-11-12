2fbda5626dd3bb49c1c0f05a663255246b65dbc7
Daniel Tran <Danieltran@hotmail.no>
2024-11-12 17:34:00 +0100

    Latest commit

--- Lab 2/scripts/read_waveform_mdo.py
+++ Lab 2/scripts/read_waveform_mdo.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
         osc.write(f":MEASure:SOURce1 CHANnel{args.mdo_input_port_in}")
         osc.write(f":MEASure:SOURce2 CHANnel{args.mdo_input_port_out}")
         time.sleep(0.5)
-        phase_shift[i] = osc.query(":MEASure:PHASe?")
+        phase_shift[i] = float(osc.query(":MEASure:PHASe?"))
         gain[i] = gain_value
         i += 1
 
