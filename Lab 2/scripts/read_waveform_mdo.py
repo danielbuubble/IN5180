7e96242fc206c54e2cea65bbd721c0b7bd5aeeb0
Daniel Tran <Danieltran@hotmail.no>
2024-11-12 17:53:56 +0100

    Latest commit

--- Lab 2/scripts/read_waveform_mdo.py
+++ Lab 2/scripts/read_waveform_mdo.py
@@ -85,7 +85,8 @@ if __name__ == "__main__":
         osc.write(f":MEASure:SOURce1 CHANnel{args.mdo_input_port_in}")
         osc.write(f":MEASure:SOURce2 CHANnel{args.mdo_input_port_out}")
         time.sleep(0.5)
-        phase_value = float(osc.query(":MEASure:phase?"))
+        phase_query_result = osc.query(":MEASure:phase?")
+        phase_value = float(phase_query_result) if phase_query_result.strip() != '?' else np.nan
 
         vpp_in.append(vpp_in_val)
         vpp_out.append(vpp_out_val)
