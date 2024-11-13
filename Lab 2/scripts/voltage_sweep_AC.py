43cdac4d4a9b76db5dc36a92997fa3d9aed4d6a9
Daniel Tran <Danieltran@hotmail.no>
2024-11-13 14:13:01 +0100

    Latest

--- Lab 2/scripts/voltage_sweep_AC.py
+++ Lab 2/scripts/voltage_sweep_AC.py
@@ -43,8 +43,8 @@ if __name__ == "__main__":
 
     i = 0
     # Configure the MFG Sweep
-    mfg.write('output'+str(args.mfg_output_port)+':load inf')
-    mfg.write('output'+str(args.mfg_output_port)+'ON')
+    #mfg.write('output'+str(args.mfg_output_port)+':load inf')
+    #mfg.write('output'+str(args.mfg_output_port)+'ON')
     mfg.write('source'+str(args.mfg_output_port)+':SWE:STAT ON')
     mfg.write('source'+str(args.mfg_output_port)+':APPL:SIN '+str(args.start_frequency)+','+str(args.amplitude)+','+str(args.offset))
     mfg.write('source'+str(args.mfg_output_port)+':FREQ:STAR '+str(args.start_frequency))
