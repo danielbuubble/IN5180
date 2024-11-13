f9d9f6a28e8155949e25baf202f15df8c112e40d
Daniel Tran <Danieltran@hotmail.no>
2024-11-13 14:43:45 +0100

    Almost

--- Lab 2/scripts/voltage_sweep_AC.py
+++ Lab 2/scripts/voltage_sweep_AC.py
@@ -54,31 +54,32 @@ if __name__ == "__main__":
     # Acquire data at each step
     for i in range(num_steps):
         # Wait for autoscale to finish
-        osc.write('AUTOSet')
+        osc.write('AUTORSET?')
         time.sleep((args.sweep_time / num_steps)/2)
-        print(f'Count: {i}')
+
         # Set MDO measurements for input
-        osc.write(':CHANnel'+str(args.mdo_input_port_in)+':DISPlay ON')
-        osc.write(':measure:source1 CH'+str(args.mdo_input_port_in))
-        in_freq_values[i] = osc.query(':measure:frequency?')
-        in_amp_values[i] = osc.query(':measure:amplitude?')
+        osc.write(f':CHANnel{args.mdo_input_port_in}:DISPlay ON')
+        osc.write(f':measure:source1 CH{args.mdo_input_port_in}')
+        in_freq_values[i] = osc.write(':measure:frequency?')
+        in_amp_values[i] = osc.write(':measure:amplitude?')
+        print(f'Cnt: {i} Frequency: {in_freq_values[i]}')
         time.sleep(0.5)
 
         # Set MDO measurements for output
-        osc.write(':CHANnel'+str(args.mdo_input_port_out)+':DISPlay ON')
-        osc.write(':measure:source2 CH'+str(args.mdo_input_port_out))
-        out_freq_values[i] = osc.query(':measure:frequency?')
-        out_amp_values[i] = osc.query(':measure:amplitude?')
+        osc.write(f':CHANnel{args.mdo_input_port_out}:DISPlay ON')
+        osc.write(f':measure:source2 CH{args.mdo_input_port_out}')
+        out_freq_values[i] = osc.write(':measure:frequency?')
+        out_amp_values[i] = osc.write(':measure:amplitude?')
 
         print(f'Amp_in: {in_amp_values[i]} Amp_out: {out_amp_values[i]}')
         time.sleep(0.5)
 
         # Measure phase shift
-        osc.write(':CHANnel'+str(args.mdo_input_port_in)+':DISPlay ON')
-        osc.write(':CHANnel'+str(args.mdo_input_port_out)+':DISPlay ON')
-        osc.write(':measure:source1 CH'+str(args.mdo_input_port_in)) #eg CH1
-        osc.write(':measure:source2 CH'+str(args.mdo_input_port_out)) #eg CH2
-        phase_shift[i] = osc.query('measure:phase?')
+        osc.write(f':CHANnel{args.mdo_input_port_in}:DISPlay ON')
+        osc.write(f':CHANnel{args.mdo_input_port_out}:DISPlay ON')
+        osc.write(f':measure:source1 CH{args.mdo_input_port_in}') # eg CH1
+        osc.write(f':measure:source2 CH{args.mdo_input_port_out}') # eg CH2
+        phase_shift[i] = osc.write('measure:phase?')
         
         time.sleep((args.sweep_time / num_steps)/2)  # Wait for the next step
 
