7d797a142c8231bf52b61b8b0611a61df3cecaa0
Daniel Tran <Danieltran@hotmail.no>
2024-11-13 14:07:44 +0100

    Latest

--- Lab 2/scripts/voltage_sweep_AC.py
+++ Lab 2/scripts/voltage_sweep_AC.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     mfg.write('source'+str(args.mfg_output_port)+':SWE:TIME ' +str(args.sweep_time))
     #mfg.write('source'+str(args.mfg_output_port)+':SWE:source IMM')
     
-    #mfg.write('source'+str(args.mfg_output_port)+':mark?')
+    mfg.write('source'+str(args.mfg_output_port)+':MARK?')
 
     # Start Sweep
     #mfg.write('OUTPUT'+str(args.mfg_output_port)+' ON')
